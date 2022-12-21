e0683c89c0fc9e707a381e02c03207f0b958f4be
MiguelDeSaAdegas <miguel.desaadegas@student.vinci.be>
2022-12-21 16:13:49 +0100

    test(checknames.test.js): add test for lowerCase names

--- services/__tests__/checkNames.test.js
+++ services/__tests__/checkNames.test.js
@@ -2,9 +2,13 @@
 const checkNames = require("../checkNames");
 
 describe("checkNames test",() => {
-    test('1: test failed',() => {
+    test('1: test name is upperCase and contains specialChars',() => {
         const result = checkNames('TRAPPIST-01');
         expect(result).toBe(true);
     });
+    test('2: test name is not upperCase',() =>{
+        const result = checkNames('trappist-01');
+        expect(result).toBeFalsy();
+    })
 
 });
